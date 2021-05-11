d209200e5151b4a3771b25b56f190cb545ca7951
Ali Hassan <alihassanuog50@gmail.com>
2021-05-11 11:07:53 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import axios from 'axios';
+import React,{Component} from 'react';
+import './App.css'
+
+class App extends Component{
+  
+  state={advice:''};
+
+  componentDidMount(){
+    this.fetchApiData();
+  }
+  fetchApiData=()=>{
+    axios.get('https://api.adviceslip.com/advice').then((response)=>{
+      const {advice}=response.data.slip;
+      this.setState({advice:advice});
+    })
+    .catch((error)=>{
+      console.log(error)
+    });
+  }
+  render(){
+    return(
+      <div className="app">
+        <div className="card">
+          <h1 className="heading">{this.state.advice}</h1>
+          <button className="button" onClick={this.fetchApiData}>
+            <span>GIVE ME ADVICE</span>
+          </button>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App;
